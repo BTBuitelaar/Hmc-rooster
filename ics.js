53f18b9ba9aad6c04cebedbc64e3f91d5275fcbc
BTBuitelaar <basbuit2001@gmail.com>
2025-11-10 20:24:51 +0100

    Update ics.js

--- ics.js
+++ ics.js
@@ -5,18 +5,21 @@ const toICSDate = dt => (
   'T' + pad(dt.getHours()) + pad(dt.getMinutes()) + '00'
 );
 
-function buildICS(events, tz='Europe/Amsterdam'){
+function buildICS(events, tz='Europe/Amsterdam') {
   const lines = [
-    'BEGIN:VCALENDAR','VERSION:2.0','CALSCALE:GREGORIAN',
-    'METHOD:PUBLISH','PRODID:-//HMC//Rooster Export//NL'
+    'BEGIN:VCALENDAR',
+    'VERSION:2.0',
+    'CALSCALE:GREGORIAN',
+    'METHOD:PUBLISH',
+    'PRODID:-//HMC//Rooster Export//NL'
   ];
   for (const e of events) {
-    const uid = `${+e.start}-${(e.title + '|' + (e.location || '')).replace(/\s+/g,'_')}@hmc`;
+    const uid = e.start.valueOf() + '-' + (e.title + '|' + (e.location || '')).replace(/\s+/g, '_') + '@hmc';
     lines.push(
       'BEGIN:VEVENT',
       `UID:${uid}`,
-      `SUMMARY:${(e.title||'Les').replace(/\r?\n/g,' ')}`,
-      `LOCATION:${e.location||''}`,
+      `SUMMARY:${(e.title || 'Les').replace(/\r?\n/g,' ')}`,
+      `LOCATION:${e.location || ''}`,
       `DTSTART;TZID=${tz}:${toICSDate(e.start)}`,
       `DTEND;TZID=${tz}:${toICSDate(e.end)}`,
       'END:VEVENT'
@@ -25,4 +28,5 @@ function buildICS(events, tz='Europe/Amsterdam'){
   lines.push('END:VCALENDAR');
   return lines.join('\r\n');
 }
+
 module.exports = { buildICS };
